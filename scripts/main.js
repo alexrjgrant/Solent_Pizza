e04442afc9df0cefa74fd9585c80fdd850654d19
alexrjgrant <alexrjgrant@hotmail.co.uk>
2017-04-04 12:29:06 +0100

    Version 5

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,30 @@ function validateForm() {
     var e = document.forms["myForm"]["email"].value;
     var m = document.forms["myForm"]["message"].value;
     
-    if (fn ||  ||   == "") {
+    if (fn == "") {
         document.getElementById("fn").style.backgroundColor = "red"
         
         return false;
     }
 }
+
+function set_time()
+{
+    var d = new Date();
+    
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    
+    if (h < 10){h = "0" + h}
+    if (m < 10){m = "0" + m}
+    if (s < 10){s = "0" + s}
+    
+    var t = (h +":"+ m + ":" + s);
+    
+    document.getElementById("time").innerHTML = t;
+    
+    
+    setInterval(set_time,500);
+}
+
