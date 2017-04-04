8f697dcd8468275b03e3fec2f2be0ae4f943d139
alexrjgrant <alexrjgrant@hotmail.co.uk>
2017-04-04 12:27:34 +0100

    Version 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
-function beans(){
+function validateForm() {
+    var fn = document.forms["myForm"]["first_name"].value;
+    var ln = document.forms["myForm"]["last_name"].value;
+    var e = document.forms["myForm"]["email"].value;
+    var m = document.forms["myForm"]["message"].value;
     
-    
+    if (fn ||  ||   == "") {
+        document.getElementById("fn").style.backgroundColor = "red"
+        
+        return false;
+    }
 }
